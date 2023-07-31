8011c06d142320a1f44138e07e11ae9bdbac5c71
Gui182 <guilherme.oliveira182@yahoo.com.br>
2023-07-30 21:29:44 -0300

    Domain Refactor

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,22 +1,24 @@
-package br.com.alura.orgs
+package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
-import org.w3c.dom.Text
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.R
+import br.com.alura.orgs.ui.recyclerView.adapter.ListaProdutosAdapter
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
-        val nome = findViewById<TextView>(R.id.nome);
+        /*val nome = findViewById<TextView>(R.id.nome);
         nome.text = "Cesta de frutas";
         val descricao = findViewById<TextView>(R.id.descricao);
         descricao.text = "Laranja, manga e maçã"
         val valor = findViewById<TextView>(R.id.valor);
-        valor.text = "19.99"
+        valor.text = "19.99"*/
+
+        val recycleView = findViewById<RecyclerView>(R.id.recyclerView);
+        recycleView.adapter = ListaProdutosAdapter();
     }
 }
