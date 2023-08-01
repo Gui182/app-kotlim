a6901a93989c208402d0c4cb857b3869f80f5216
Gui182 <guilherme.oliveira182@yahoo.com.br>
2023-07-31 21:12:19 -0300

    Add formulario produto view

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,8 @@
+package br.com.alura.orgs.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+}
--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -2,9 +2,12 @@ package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerView.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
@@ -19,6 +22,20 @@ class MainActivity : Activity() {
         valor.text = "19.99"*/
 
         val recycleView = findViewById<RecyclerView>(R.id.recyclerView);
-        recycleView.adapter = ListaProdutosAdapter();
+        recycleView.adapter = ListaProdutosAdapter(
+            context = this, produtos = listOf(
+                Produto(
+                    nome = "teste",
+                    descricao = "teste desc",
+                    valor = BigDecimal("19.99")
+                ),
+                Produto(
+                    nome = "teste1",
+                    descricao = "teste desc1",
+                    valor = BigDecimal("29.99")
+                ),
+            )
+        );
+        //recycleView.layoutManager = LinearLayoutManager(this);
     }
 }
