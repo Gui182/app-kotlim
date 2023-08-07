7008dd315014f4a160a20c19f78306a0ded0a902
Gui182 <guilherme.oliveira182@yahoo.com.br>
2023-08-07 19:57:41 -0300

    Code Refactoring

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,8 +1,6 @@
 package br.com.alura.orgs.ui.activity
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -16,32 +14,36 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        configuraBotaoSalvar()
+    }
 
-        val botaoSalvar = findViewById<Button>(R.id.botao_salvar);
+    private fun configuraBotaoSalvar() {
+        val botaoSalvar = findViewById<Button>(R.id.activity_formulario_produto_botao_salvar);
         botaoSalvar.setOnClickListener(View.OnClickListener {
-            val campoNome = findViewById<EditText>(R.id.nome);
-            val nome = campoNome.text.toString();
-            val campoDescricao = findViewById<EditText>(R.id.descricao);
-            val descricao = campoDescricao.text.toString();
-            val campoValor = findViewById<EditText>(R.id.valor);
-            val valorEmTexto = campoValor.text.toString();
-            val valor = if(valorEmTexto.isBlank()) {
-                BigDecimal.ZERO
-            } else {
-                BigDecimal(valorEmTexto)
-            }
-
-
-            val produtoNovo = Produto(
-                nome = nome,
-                descricao = descricao,
-                valor = valor
-            )
-
-            Log.i("FormularioProduto", "onCreate $produtoNovo")
+            val produtoNovo = criaProduto()
             val dao = ProdutosDao();
             dao.adiciona(produtoNovo);
-            Log.i("FormularioProduto", "onCreate ${dao.buscaTodos()}")
+            finish();
         })
+    }
+
+    private fun criaProduto(): Produto {
+        val campoNome = findViewById<EditText>(R.id.activity_formulario_produto_nome);
+        val nome = campoNome.text.toString();
+        val campoDescricao = findViewById<EditText>(R.id.activity_formulario_produto_descricao);
+        val descricao = campoDescricao.text.toString();
+        val campoValor = findViewById<EditText>(R.id.activity_formulario_produto_valor);
+        val valorEmTexto = campoValor.text.toString();
+        val valor = if (valorEmTexto.isBlank()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(valorEmTexto)
+        }
+
+        return Produto(
+            nome = nome,
+            descricao = descricao,
+            valor = valor
+        )
     }
 }
---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -0,0 +1,47 @@
+package br.com.alura.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.R
+import br.com.alura.orgs.dao.ProdutosDao
+import br.com.alura.orgs.ui.recyclerView.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos) {
+
+    val dao = ProdutosDao();
+    private val adapter = ListaProdutosAdapter(
+        context = this, produtos = dao.buscaTodos()
+    );
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        configureRecycleView()
+        configuraFab()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.atualiza(dao.buscaTodos())
+    }
+
+    private fun configuraFab() {
+        val fab = findViewById<FloatingActionButton>(R.id.activity_lista_produtos_fab);
+        fab.setOnClickListener(View.OnClickListener {
+            vaiParaFormularioProduto();
+        })
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun configureRecycleView() {
+        val recycleView = findViewById<RecyclerView>(R.id.activity_lista_produtos_recyclerView);
+        recycleView.adapter = adapter;
+    }
+}
